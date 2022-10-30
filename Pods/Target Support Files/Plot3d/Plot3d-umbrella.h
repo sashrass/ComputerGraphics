1a48f19457cbd5c8582677e400634deb15241844
sashrass <alexandrultra@gmail.com>
2022-10-30 23:09:31 +0300

    additional files

---
+++ Pods/Target Support Files/Plot3d/Plot3d-umbrella.h
@@ -0,0 +1,17 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+#import "Plot3D.h"
+
+FOUNDATION_EXPORT double Plot3dVersionNumber;
+FOUNDATION_EXPORT const unsigned char Plot3dVersionString[];
+
