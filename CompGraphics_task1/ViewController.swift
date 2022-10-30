de9bfa70f63749d93144873f3d1dd62c75b8f49b
sashrass <alexandrultra@gmail.com>
2022-10-30 23:07:23 +0300

    lab 3 initial commit

--- CompGraphics_task1/ViewController.swift
+++ CompGraphics_task1/ViewController.swift
@@ -1,163 +1,244 @@
+//
+//  ViewController.swift
+//  CompGraph_la3
+//
+//  Created by Александр Рассохин on 28.10.2022.
+//
+
 import UIKit
+import simd
+import Plot3d
+import SceneKit
 
 class ViewController: UIViewController {
     
-    let bezierView = BezierView()
+    let a = 15
+    var points: [Point] = []
+    var startPoints = [Point]()
+    var plotView: PlotView?
+    
+    let xSlider: UISlider = UISlider()
+    let ySlider: UISlider = UISlider()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .white
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .black
+        view.addSubview(xSlider)
+        view.addSubview(ySlider)
         
-        view.addSubview(bezierView)
-        bezierView.translatesAutoresizingMaskIntoConstraints = false
+        xSlider.translatesAutoresizingMaskIntoConstraints = false
+        ySlider.translatesAutoresizingMaskIntoConstraints = false
+        ySlider.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10).isActive = true
+        ySlider.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 10).isActive = true
+        ySlider.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -5).isActive = true
         
-        NSLayoutConstraint.activate([
-            bezierView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            bezierView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            bezierView.topAnchor.constraint(equalTo: view.topAnchor),
-            bezierView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
-        ])
-                
-        let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(gestureRecognized(sender:)))
+        xSlider.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10).isActive = true
+        xSlider.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 10).isActive = true
+        xSlider.bottomAnchor.constraint(equalTo: ySlider.topAnchor, constant: -10).isActive = true
         
-        view.addGestureRecognizer(gestureRecognizer)
-    }
-    
-    var amountOfAddedPoints = 0
-    @objc func gestureRecognized(sender: UITapGestureRecognizer) {
-        guard amountOfAddedPoints < 4 else { return }
+        xSlider.addTarget(self, action: #selector(rotateByX), for: .touchUpOutside)
+        ySlider.addTarget(self, action: #selector(rotateByY), for: .touchUpOutside)
+        xSlider.addTarget(self, action: #selector(rotateByX), for: .touchUpInside)
+        ySlider.addTarget(self, action: #selector(rotateByY), for: .touchUpInside)
         
-        let location = sender.location(in: view)
+        let createdPoints = createPoints(count: 4, start: 6, stop: 12)
         
-        let pointView = UIView()
-        pointView.frame.size.width = 5
-        pointView.frame.size.height = 5
-        pointView.center = location
-        pointView.tag = amountOfAddedPoints
-        let panGestureRecognizer = UIPanGestureRecognizer(target: self, action: #selector(panGestureRecognized))
-        pointView.addGestureRecognizer(panGestureRecognizer)
+        // [Point(x: 2.7, y: -1.4, z: 0.8), Point(x: -2.6, y: -1.8, z: 2.7), Point(x: -2.4, y: 2.3, z: -1.9), Point(x: -2.7, y: -1.5, z: 1.5)]
         
-        pointView.backgroundColor = .black
-        view.addSubview(pointView)
-        
-        switch amountOfAddedPoints {
-        case 0:
-            bezierView.point0 = location
-        case 1:
-            bezierView.point1 = location
-        case 2:
-            bezierView.point2 = location
-        case 3:
-            bezierView.point3 = location
-        default:
-            break
-        }
-        
-        amountOfAddedPoints += 1
-        
-        if amountOfAddedPoints == 4 {
-            bezierView.setNeedsDisplay()
-        }
-        
-    }
-    
-    var dragStartPoint: CGPoint?
-    @objc func panGestureRecognized(gesture: UIPanGestureRecognizer) {
-        
-        guard amountOfAddedPoints == 4,
-              let pointView = gesture.view else {return}
-        
-        switch gesture.state {
-        case .began:
-            dragStartPoint = pointView.frame.origin
-        case .changed:
-            let xTranslation = gesture.translation(in: self.view).x
-            let yTranslation = gesture.translation(in: self.view).y
+        let pts = createPointsForSurface(vertices: createdPoints)
+        self.points = pts.horizontal.flatMap {$0} + pts.vertical.flatMap {$0}
+        self.startPoints = self.points
 
-            pointView.center.x = dragStartPoint!.x + xTranslation
-            pointView.center.y = dragStartPoint!.y + yTranslation
-            
-            switch pointView.tag {
-            case 0:
-                bezierView.point0 = pointView.center
-            case 1:
-                bezierView.point1 = pointView.center
-            case 2:
-                bezierView.point2 = pointView.center
-            case 3:
-                bezierView.point3 = pointView.center
-            default:
-                break
+        let config = PlotConfiguration()
+
+        let frame = CGRect(x: 0, y: 0, width: view.bounds.width, height: view.bounds.height - 150)
+        let plotView = PlotView(frame: frame, configuration: config)
+        self.plotView = plotView
+        view.addSubview(plotView)
+        
+        plotView.setCamera(position: PlotPoint(10, 6, 10))
+        plotView.setCamera(lookAt: PlotPoint(0, 0, 0))
+
+        plotView.setAxisTitle(.x, text: "x axis", textColor: .white)
+        plotView.setAxisTitle(.y, text: "y axis", textColor: .white)
+        plotView.setAxisTitle(.z, text: "z axis", textColor: .white)
+        
+        plotView.dataSource = self
+        plotView.delegate = self
+        plotView.reloadData()
+    }
+
+    func createPoints(count: Int, start: Float, stop: Float, step: Float = 0.1) -> [Point]{
+        
+        var points = [Point]()
+        var numbersList: [Float] = []
+        var result = start
+        
+        while result < stop {
+            numbersList.append(result.rounded(toPlaces: 1))
+            result += step
+        }
+        
+        for _ in 0..<count {
+            points.append(Point(x: numbersList.randomElement()!, y: numbersList.randomElement()!, z: numbersList.randomElement()!))
+        }
+        
+        return points
+    }
+    
+    func makeXRotationMatrix(angle: Float) -> simd_float3x3 {
+        let rows = [
+            simd_float3(1,      0,               0),
+            simd_float3(0, cos(angle), -sin(angle)),
+            simd_float3(0, sin(angle),  cos(angle))
+        ]
+
+        return float3x3(rows: rows)
+    }
+    
+    func makeYRotationMatrix(angle: Float) -> simd_float3x3 {
+        let rows = [
+            simd_float3(cos(angle), 0, sin(angle)),
+            simd_float3(0,           1,         0),
+            simd_float3(-sin(angle), 0,  cos(angle))
+        ]
+
+        return float3x3(rows: rows)
+    }
+    
+    func createPointsForSurface(vertices: [Point]) -> (horizontal: [[Point]], vertical: [[Point]]) {
+        
+        var points: (horizontal: [[Point]], vertical: [[Point]]) = ([], [])
+     
+        for i in 0...a {
+            points.horizontal.append([])
+            let u = Float(i) / Float(a)
+            for j in 0...a {
+                let w = Float(j) / Float(a)
+                let x = (1 - u) * (1 - w) * vertices[0].x + (1 - u) * w * vertices[1].x + u * (1 - w) * vertices[2].x + u * w * vertices[3].x
+                let y = (1 - u) * (1 - w) * vertices[0].y + (1 - u) * w * vertices[1].y + u * (1 - w) * vertices[2].y + u * w * vertices[3].y
+                let z = (1 - u) * (1 - w) * vertices[0].z + (1 - u) * w * vertices[1].z + u * (1 - w) * vertices[2].z + u * w * vertices[3].z
+
+                let point = Point(x: x, y: y, z: z)
+                points.horizontal[i].append(point)
             }
-            
-            bezierView.setNeedsDisplay()
-        case.ended:
-            dragStartPoint = nil
-            
-        default:
-            break
+        }
+        
+        for i in 0...a {
+            points.vertical.append([])
+            let w = Float(i) / Float(a)
+            for j in 0...a {
+                let u = Float(j) / Float(a)
+                let x = (1 - u) * (1 - w) * vertices[0].x + (1 - u) * w * vertices[1].x + u * (1 - w) * vertices[2].x + u * w * vertices[3].x
+                let y = (1 - u) * (1 - w) * vertices[0].y + (1 - u) * w * vertices[1].y + u * (1 - w) * vertices[2].y + u * w * vertices[3].y
+                let z = (1 - u) * (1 - w) * vertices[0].z + (1 - u) * w * vertices[1].z + u * (1 - w) * vertices[2].z + u * w * vertices[3].z
+
+                let point = Point(x: x, y: y, z: z)
+                points.vertical[i].append(point)
+            }
+        }
+
+        return points
+    }
+    
+    @objc func rotateByX() {
+        ySlider.value = 0
+        let angle = (360 * xSlider.value) * .pi / 180
+        print("angle: \(angle)")
+        let rotationMatrix = makeXRotationMatrix(angle: angle)
+        
+        let rotatedPoints = startPoints.map { point -> Point in
+            let rotated = rotationMatrix * simd_float3(x: point.x, y: point.y, z: point.z)
+            return Point(x: rotated.x, y: rotated.y, z: rotated.z)
+        }
+        self.points = rotatedPoints
+        plotView?.reloadData()
+    }
+    
+    @objc func rotateByY() {
+        xSlider.value = 0
+        let angle = (360 * ySlider.value) * .pi / 180
+        print("angle: \(angle)")
+        let rotationMatrix = makeYRotationMatrix(angle: angle)
+        
+        let rotatedPoints = startPoints.map { point -> Point in
+            let rotated = rotationMatrix * simd_float3(x: point.x, y: point.y, z: point.z)
+            return Point(x: rotated.x, y: rotated.y, z: rotated.z)
+        }
+        self.points = rotatedPoints
+        plotView?.reloadData()
+    }
+}
+
+
+extension ViewController: PlotDataSource, PlotDelegate {
+    func plot(_ plotView: Plot3d.PlotView, pointForItemAt index: Int) -> Plot3d.PlotPoint {
+        
+        return points[index].plotPoint
+    }
+    
+    func numberOfPoints() -> Int {
+        print("points count = \(points.count)")
+        return points.count
+    }
+    
+    func plot(_ plotView: Plot3d.PlotView, geometryForItemAt index: Int) -> SCNGeometry? {
+        let geo = SCNSphere(radius: 0.01)
+        geo.materials.first!.diffuse.contents = UIColor.red
+        return geo
+    }
+    
+    func plot(_ plotView: PlotView, textAtTickMark index: Int, forAxis axis: PlotAxis) -> PlotText? {
+        let config = PlotConfiguration()
+        switch axis {
+        case .x:
+            return PlotText(text: "\(index + 1)", fontSize: 0.3, offset: 0.25)
+        case .y:
+            return PlotText(text: "\(Int(CGFloat(index + 1) * config.yTickInterval))", fontSize: 0.3, offset: 0.1)
+        case .z:
+            return PlotText(text: "\(index + 1)", fontSize: 0.3, offset: 0.25)
         }
     }
     
+    func numberOfConnections() -> Int {
+        return points.count
+    }
+    
+    //Connect the points in a way that creates a double helix.
+    func plot(_ plotView: PlotView, pointsToConnectAt index: Int) -> (p0: Int, p1: Int)? {
+        print("index + 1: \(index + 1)")
+        if (index + 1) % (a+1) == 0 {
+            print("nil")
+            return nil
+        }
+        else {
+            return (p0: index, p1: index + 1)
+        }
+
+    }
+
+    //Define the geometry of each connection.
+    func plot(_ plotView: PlotView, connectionAt index: Int) -> PlotConnection? {
+        return PlotConnection(radius: 0.01, color: .green)
+    }
 
 }
 
-class BezierView: UIView {
-    var point0: CGPoint = CGPoint(x: 0, y: 0)
-    var point1: CGPoint = CGPoint(x: 0, y: 0)
-    var point2: CGPoint = CGPoint(x: 0, y: 0)
-    var point3: CGPoint = CGPoint(x: 0, y: 0)
+struct Point {
+    var x: Float
+    var y: Float
+    var z: Float
     
-    private let bezierLayer = CAShapeLayer()
-    
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        layer.addSublayer(bezierLayer)
-        bezierLayer.fillColor = nil
-        bezierLayer.strokeColor = UIColor.black.cgColor
-        bezierLayer.lineWidth = 2
-        backgroundColor = .white
+    var plotPoint: PlotPoint {
+        return PlotPoint(CGFloat(x), CGFloat(y), CGFloat(z))
     }
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-        
-    override func draw(_ rect: CGRect) {
-        drawPolyline()
-        
-        let path = UIBezierPath()
-        
-        path.move(to: point0)
-        for t in stride(from: 0, to: 1, by: 0.001) {
-            let finalPoint = getPointForCubicBezier(t: t)
-            path.addLine(to: finalPoint)
-        }
-        
-        bezierLayer.path = path.cgPath
-    }
-    
-    func drawPolyline() {
-        let path = UIBezierPath()
-        path.move(to: point0)
-        path.addLine(to: point1)
-        path.addLine(to: point2)
-        path.addLine(to: point3)
-        UIColor.gray.set()
-        path.stroke()
-    }
-    
-    func getPointForCubicBezier(t: Double) -> CGPoint {
-        var finalPoint = CGPoint()
-        finalPoint.x = pow(1 - t, 3) * point0.x +
-                           pow(1 - t, 2) * 3 * t * point1.x +
-                           (1 - t) * 3 * t * t * point2.x +
-                           t * t * t * point3.x;
-        finalPoint.y = pow(1 - t, 3) * point0.y +
-                           pow(1 - t, 2) * 3 * t * point1.y +
-                           (1 - t) * 3 * t * t * point2.y +
-                           t * t * t * point3.y;
-        
-        return finalPoint
+}
+
+extension Float {
+
+    func rounded(toPlaces places:Int) -> Float {
+        let divisor = pow(10.0, Float(places))
+        return (self * divisor).rounded() / divisor
     }
 }
